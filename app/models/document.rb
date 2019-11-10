c34c516c3a27e3e1b2ee8d587b1807e2fabd9428
jc7v <jc7v@riseup.net>
2019-11-10 08:33:39 +0000

    Rails admin install and validation on dicument

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,9 +1,19 @@
 class Document < ActiveRecord::Base
   belongs_to :user
 
-  rails_admin do
-    configure :player do
-      label 'Owner of this ball: '
+  validates_presence_of :title, :doc_asset
+  validates :number_of_pages, numericality: {greater_than_or_equal_to: 0}
+  validates :author, length: {maximum: 50}
+  validate :realized_at_before_today
+
+  private
+
+  def realized_at_before_today
+    return if realized_at.blank?
+
+    if realized_at > Date.today
+      errors.add(:realized_at, t('documents.errors.realized_at'))
     end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'document#index'
   resources :document, except: [:index]
 
