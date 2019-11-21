69d058b4f3edb7b011090d31631c07040adf31f2
haiory@protonmail.com <haiory@protonmail.com>
2019-11-21 19:52:37 +0100

    Add tests for validation of a document

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,11 +4,13 @@ class Document < ApplicationRecord
   has_one_attached :doc_asset
   enum status: [:refused, :accepted]
 
-  validates_presence_of :title, :doc_asset
-  #TODO add default to 0
+  before_validation :set_number_of_pages_to_0
+
+  validates_presence_of :title
   validates :number_of_pages, numericality: {greater_than_or_equal_to: 0}
   validates :author, length: {maximum: 50}
   validate :realized_at_before_today
+  validate :doc_asset_presence
 
   private
 
@@ -18,6 +20,14 @@ class Document < ApplicationRecord
     if realized_at > Date.today
       errors.add(:realized_at, I18n.t('documents.errors.realized_at'))
     end
+  end
+
+  def doc_asset_presence
+    errors.add(:doc_asset, I18n.t('documents.errors.doc_asset.presence')) unless self.doc_asset.attached?
+  end
+
+  def set_number_of_pages_to_0
+    self.number_of_pages = 0 if number_of_pages.blank?
   end
 
 end
--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -4,8 +4,14 @@ class DocumentTest < ActiveSupport::TestCase
 
   class ActiveSupport::TestCase::With
     def assert_not_save(method, value)
-      super(documents(:doc1), method, value)
+      d = documents(:doc1)
+      d.doc_asset.attach(io: file_fixture('test.pdf').open, filename: 'test.pdf')
+      super(d, method, value)
     end
+  end
+
+  def attach_asset(document)
+    document.doc_asset.attach(io: file_fixture('test.pdf').open, filename: 'test.pdf')
   end
 
   should_not_save do |with|
@@ -28,20 +34,43 @@ class DocumentTest < ActiveSupport::TestCase
     with.a 'nil user' do
       assert_not_save :user=, nil
     end
+
+    with.a 'nil asset' do
+      assert_not documents(:doc1).save
+    end
   end
 
   test 'author can be blank' do
     d = documents(:doc1)
+    attach_asset d
     d.author = ''
     assert d.save
   end
 
-  test 'Document has a user' do
-    d = Document.new(title: 'asd', number_of_pages: 1)
-    d.user = users(:user1)
-    d.save!
-    assert d.reload.user_id = users(:user1).id
+  test 'number of pages has 0 value by default' do
+    d = documents(:doc1)
+    d.number_of_pages = nil
+    attach_asset d
+    d.save
+    assert_equal 0, d.number_of_pages
   end
 
+  test 'belongs to a user' do
+    d = Document.new(title: 'asd')
+    d.user = users(:user1)
+    attach_asset d
+    d.save
+    assert_equal d.reload.user_id, users(:user1).id
+  end
 
+  test 'has many tags' do
+    d = Document.new(title: 'title')
+    d.user = users(:user1)
+    attach_asset d
+    doc_tags = tags(:tag1, :tag2, :tag3)
+    d.tags  = doc_tags
+    tag_ids = doc_tags.map {|t| t.id}
+    d.save
+    assert_equal(d.reload.tag_ids, tag_ids)
+  end
 end
