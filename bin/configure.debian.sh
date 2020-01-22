1db6f720011204d4f4fa6b7dbc11db790251f182
jc7v <jc7v@riseup.net>
2020-01-22 14:12:21 +0000

    update firewall rules in config script

--- bin/configure.debian.sh
+++ bin/configure.debian.sh
@@ -4,6 +4,11 @@
 
 sudo bash -c "
 apt install  default-jre midori imagemagick\
-iptables -I OUTPUT -d localhost -p tcp --dport 8983 -j ACCEPT
+iptables -I OUTPUT -d localhost -o lo -p tcp --dport 8983 -m owner --uid-owner 1000 -j ACCEPT
 "
 
+# To be able to go on 192.168.0.0/24
+# iptables -I OUTPUT -d 192.168.0.0/24 -o lo -p tcp --dport 3000 -m owner --uid-owner 1000 -j ACCEPT
+
+# To be abl to be accessible from the local network
+# iptables -I INPUT -p tcp --dport 3000 -s 192.168.0.0/24 -j ACCEPT
